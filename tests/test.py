cbbfeaf4551f6a62fcc7adb96c5826b5c54f32dc
yan <yan@email.com>
2025-07-25 12:05:35 +0300

    updated workflows for relevant stuff

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestDownloadingModule(unittest.TestCase):
+    def testClassSetUp(self):
+        pass
+
+    def first_test(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
